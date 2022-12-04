4b21777c02e4e04befe6e28a77f1b62693727340
REALSTEVEIG <101066723+REALSTEVEIG@users.noreply.github.com>
2022-12-04 21:01:22 +0100

    connection string

--- db/connect.js
+++ db/connect.js
@@ -7,11 +7,13 @@ const Sequelize = require('sequelize')
 // })
 // npm install --save pg pg-hstore
 
-const sequelize = new Sequelize('sequelize', 'root', 'password', {
-    host :  'localhost',
-    dialect : 'mysql',
-    // logging: (...msg) => console.log(msg),
-})
+// const sequelize = new Sequelize('sequelize', 'root', 'password', {
+//     host :  'localhost',
+//     dialect : 'mysql',
+//     // logging: (...msg) => console.log(msg),
+// })
 //npm install --save mysql2
+
+const sequelize = new Sequelize('mysql://ukihhc3aiannxdgy:vZnYWqjDylvTVDn0mYDE@bsfh0cq02jl3mqkau4kt-mysql.services.clever-cloud.com:3306/bsfh0cq02jl3mqkau4kt')
 
 module.exports = sequelize
