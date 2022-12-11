25e2e7ae6e92f003ee6879d8a7ffb01a2389756e
REALSTEVEIG <101066723+REALSTEVEIG@users.noreply.github.com>
2022-12-11 19:12:02 +0100

    final commit

--- db/connect.js
+++ db/connect.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize')
+require('dotenv').config()
 
 // const sequelize = new Sequelize('Rest_API', 'postgres', '1738', {
 //     host :  'localhost',
@@ -14,6 +15,26 @@ const Sequelize = require('sequelize')
 // })
 //npm install --save mysql2
 
-const sequelize = new Sequelize('mysql://ukihhc3aiannxdgy:vZnYWqjDylvTVDn0mYDE@bsfh0cq02jl3mqkau4kt-mysql.services.clever-cloud.com:3306/bsfh0cq02jl3mqkau4kt')
+// const sequelize = new Sequelize('mysql://ukihhc3aiannxdgy:vZnYWqjDylvTVDn0mYDE@bsfh0cq02jl3mqkau4kt-mysql.services.clever-cloud.com:3306/bsfh0cq02jl3mqkau4kt')
+//pass the connection string from clever cloud or aws or google cloud
+
+
+//USING AWS FOR POSTGRESQL
+// const sequelize = new Sequelize(
+//     process.env.POSTGRESQL_NAME, 
+//     process.env.POSTGRESQL_ADMIN,
+//     process.env.POSTGRESQL_PASSWORD, {
+//     host :  process.env.POSTGRESQL_DB_HOST,
+//     dialect : 'postgres'
+// })
+
+//USING AWS FOR MYSQL
+const sequelize = new Sequelize(
+    process.env.MYSQL_DB_NAME,
+    process.env.MYSQL_DB_ADMIN,
+    process.env.MYSQL_DB_PASSWORD, {
+    host :  process.env.MYSQL_DB_HOST,
+    dialect : 'mysql'
+})
 
 module.exports = sequelize
